718701eccaa4f7b7d8c9e862cf9c72db56872e0a
Fountain <fountain@Fountains-MacBook-Pro.local>
2025-02-12 12:42:33 -0600

    grayscale

--- cms_map.js
+++ cms_map.js
@@ -14,7 +14,7 @@ function createmap(e) {
                 tittle: n[M].innerHTML,
                 lat: Number(t[M].innerHTML),
                 lng: Number(s[M].innerHTML),
-                icon: "https://cdn.prod.website-files.com/65806d348e62b54ace7108bb/67ace756c6eeee9f65e16f19_TVCR_Location_Marker64.png", // Default marker image
+                icon: "https://cdn.prod.website-files.com/65806d348e62b54ace7108bb/67ace756c6eeee9f65e16f19_TVCR_Location_Marker64.png", // Custom marker
                 contdiv: '<div class="info-box"><img class="projimg" src="' + m + 
                          '"/><p class="projname">' + n[M].innerHTML + 
                          '</p><p class="projdesc">' + o[M].innerHTML + "</p></div>"
@@ -22,9 +22,15 @@ function createmap(e) {
         }
     }
 
+    // Initialize the map with greyscale styles
     const g = new google.maps.Map(document.getElementById("map-div"), {
         zoom: 9,
-        center: a[0]
+        center: a[0],
+        styles: [
+            { elementType: "geometry", stylers: [{ saturation: -100 }] }, // Full greyscale
+            { elementType: "labels.icon", stylers: [{ visibility: "off" }] }, // Hides Google icons
+            { featureType: "road", elementType: "geometry", stylers: [{ saturation: -100 }] } // Greyscale roads
+        ]
     });
 
     const p = new google.maps.InfoWindow;
@@ -35,8 +41,8 @@ function createmap(e) {
             map: g,
             title: a[M].tittle,
             icon: {
-                url: a[M].icon, // Uses the specified custom icon
-                scaledSize: new google.maps.Size(40, 40) // Adjust the size as needed
+                url: a[M].icon, // Custom icon
+                scaledSize: new google.maps.Size(40, 40) // Adjust the size if needed
             }
         });
 
